7de611b8f72fa61a71c42a704b70aefbf85beefa
Youngmoon Oh <moony_bb@naver.com>
2022-10-06 22:53:12 +0900

    20221006_오영문 : mybatis-mapper 적용 및 테스트 완료

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,26 @@ var router = express.Router();
 /* maria require - 20221006_moon */
 const maria = require('../database/connect/maria');
 
+/* common-mapper */
+const mybatisMapper = require('mybatis-mapper');
+mybatisMapper.createMapper(['./database/mapper/common-mapper.xml']);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '꼬북' });
 });
 
+// SQL parameter
+var param = {
+  csNo : '202210060001'
+};
+
+var format = {language: 'sql', indent: '  '};
+var query = mybatisMapper.getStatement('csInf', 'selectCsInf', param, format);
+
+
 router.get('/select', function(req, res, next) {
-  maria.query('select * from tm_cs', function(err, rows, fields) {
+  maria.query(query, function(err, rows, fields) {
     if(!err) {
       console.log("succ");
       res.send(rows);
@@ -19,5 +32,7 @@ router.get('/select', function(req, res, next) {
     }
   });
 });
+
+
 
 module.exports = router;
